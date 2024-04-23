1eed6a5b8327bd5afde376e85c791fc3f7cf1a59
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-23 12:07:49 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
+const { text } = require('express');
 const net = require('net');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log('Logs from your program will appear here!');
+//console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
@@ -13,7 +14,7 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         httpRequest = data.toString();
 
-        console.log(httpRequest);
+        console.log("This is the full http request \n"+httpRequest);
         const path = httpRequest.split(' ');
         console.log(path[1]);
 
@@ -21,6 +22,7 @@ const server = net.createServer((socket) => {
             socket.write('HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n');
         } 
         else if(path.startsWith('/echo')){
+            
             socket.write('HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n Content-Length: \r\n\r\nHello');
 
         }
