6356b5695f94ab940206326ad370388792ec15c3
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-23 11:40:11 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ const server = net.createServer((socket) => {
     
     socket.on("data", (data) => {
         httpRequest = data.toString();
-
+        console.log(data);
         const [path] = httpRequest.split(" ");
         console.log(path);
         if (path === "/") {
