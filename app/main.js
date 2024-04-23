237f52a38ff3b448f70a1673707807ce0dfa1288
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-23 15:21:49 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -9,34 +9,34 @@ const server = net.createServer((socket) => {
 
 server.listen(4221, "localhost");
 server.on('connection', function(socket) {
-socket.on('data', (chunk) => {
-    console.log('data received from the client: ', chunk.toString().split('\r\n'))
-    const path = chunk.toString().split('\r\n')[0].split(' ')[1]
-    if (path === '/') {
-        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.on('data', (chunk) => {
+        console.log('data received from the client: ', chunk.toString().split('\r\n'))
+        const path = chunk.toString().split('\r\n')[0].split(' ')[1]
+        if (path === '/') {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n');
 
-    } else if (path.startsWith('/echo')) {
-        let arr = path.split('/').filter(elem => elem)
-        arr.shift()
-        const EchoString = arr.join('/')
-        let resp = 'HTTP/1.1 200 OK\r\n'
-        resp += 'Content-Type: text/plain\r\n'
-        resp += `Content-Length: ${EchoString.length}\r\n\r\n${EchoString}`
-        socket.write(resp);
-        return
+        } else if (path.startsWith('/echo')) {
+            let arr = path.split('/').filter(elem => elem)
+            arr.shift()
+            const EchoString = arr.join('/')
+            let resp = 'HTTP/1.1 200 OK\r\n'
+            resp += 'Content-Type: text/plain\r\n'
+            resp += `Content-Length: ${EchoString.length}\r\n\r\n${EchoString}`
+            socket.write(resp);
+            return
 
-    }else if(path.startsWith('/user-agent')){
-        console.log('We got to the user agent area')
-        console.log(chunk)
-        /*const UseragentStr;
-        let resp = 'HTTP/1.1 200 OK\r\n'
-        resp += 'Content-Type: text/plain\r\n'
-        resp += `Content-Length: ${UseragentStr.length}\r\n\r\n${UseragentStr}`
-        socket.write(resp);*/
-        return
-    } else {
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-        return
+        }else if(path.startsWith('/user-agent')){
+            console.log('We got to the user agent area')
+            console.log(chunk.toString().split('\r\n'))
+            /*const UseragentStr;
+            let resp = 'HTTP/1.1 200 OK\r\n'
+            resp += 'Content-Type: text/plain\r\n'
+            resp += `Content-Length: ${UseragentStr.length}\r\n\r\n${UseragentStr}`
+            socket.write(resp);*/
+            return
+        } else {
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            return
         }
     })
 })
