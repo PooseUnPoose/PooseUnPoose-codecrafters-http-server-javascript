d19a69ca632c17ff9df7c32e1e7fd728836d0225
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-23 15:34:13 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -17,25 +17,23 @@ server.on('connection', function(socket) {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
 
         } else if (path.startsWith('/echo')) {
-            let arr = path.split('/').filter(elem => elem)
-            arr.shift()
-            const EchoString = arr.join('/')
-            let resp = 'HTTP/1.1 200 OK\r\n'
-            resp += 'Content-Type: text/plain\r\n'
-            resp += `Content-Length: ${EchoString.length}\r\n\r\n${EchoString}`
+            const EchoString = (chunk.toString().split('\r\n')[0].split(' ')[1]);;
+            let resp = 'HTTP/1.1 200 OK\r\n';
+            resp += 'Content-Type: text/plain\r\n';
+            resp += `Content-Length: ${EchoString.length}\r\n\r\n${EchoString}`;
             socket.write(resp);
             return
 
         }else if(path.startsWith('/user-agent')){
-            console.log('We got to the user agent area')
+            console.log('We got to the user agent area');
             const UseragentStr = (chunk.toString().split('\r\n')[2].split(' ')[1]);
-            let resp = 'HTTP/1.1 200 OK\r\n'
-            resp += 'Content-Type: text/plain\r\n'
-            resp += `Content-Length: ${UseragentStr.length}\r\n\r\n${UseragentStr}`
+            let resp = 'HTTP/1.1 200 OK\r\n';
+            resp += 'Content-Type: text/plain\r\n';
+            resp += `Content-Length: ${UseragentStr.length}\r\n\r\n${UseragentStr}`;
             socket.write(resp);
             return
         }else if(path.startsWith('/files')){
-            console.log("we got to the files area")
+            console.log("we got to the files area");
             return
         } else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
