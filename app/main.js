cc30abe20eb7ac068b2ebd21ab9ca386551b3891
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-24 13:02:07 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
 const net = require('net');
-
 const fs = require('fs');
 
 console.log('Logs from your program will appear here!');
@@ -8,18 +7,20 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const [requestLine, ...headers] = data.toString().split('\r\n');
         const [method, path] = requestLine.split(' ');
+
         if (path === '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
         } else if (path.startsWith('/echo/')) {
-            handleEchoRequest(path, socket);
+            EchoRequest(path, socket);
         } else if (path.startsWith('/user-agent')) {
-            handleAgentRequest(headers, socket);
+            AgentRequest(headers, socket);
         } else if (path.startsWith('/files/')) {
-            handleFileRequest(path, socket);
+            FileRequest(path, socket);
         } else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
             return;
         }
+
         socket.end();
     });
     socket.on('close', () => {
@@ -29,16 +30,15 @@ const server = net.createServer((socket) => {
 
 server.listen(4221, 'localhost');
 
-function handleEchoRequest(path, socket) {
-    const randomString = path.substring('/echo/'.length);
-    const contentLength = randomString.length;
-    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${randomString}\r\n`;
+function EchoRequest(path, socket) {
+    const EchoString = path.substring('/echo/'.length);
+    const EchoLength = EchoString.length;
+    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${EchoLength}\r\n\r\n${EchoString}\r\n`;
     socket.write(response);
-
 }
 
-function handleAgentRequest(headers, socket) {
-    let userAgent = 'Unknown';
+function AgentRequest(headers, socket) {
+    let userAgent = '';
     for (const header of headers) {
         if (header.startsWith('User-Agent:')) {
             userAgent = header.substring('User-Agent:'.length).trim();
@@ -48,7 +48,21 @@ function handleAgentRequest(headers, socket) {
     const contentLength = userAgent.length;
     console.log(userAgent);
     const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${userAgent}\r\n`;
-    // Send the response
     socket.write(response);
+}
 
+function FileRequest(path, socket) {
+    const FileName = path.substring('/files/'.length);
+    const Directory = process.argv[process.argv,indexOf('--directory') + 1];
+    const FilePath = `${Directory}/${FileName}`;
+
+    if (fs.existsSync(FilePath)) {
+        const FileContent = fs.readFileSync(FilePath);
+        const FileLength = FileContent.length;
+        const Response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-length: ${FileLength}`;
+        socket.write(Response);
+        socket.write(FileContent);
+    }else{
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
 }
