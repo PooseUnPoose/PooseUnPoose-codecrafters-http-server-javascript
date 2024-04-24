b22e166e1221dac7abe9b827a5143b870519338d
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-24 14:03:56 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const [requestLine, ...headers] = data.toString().split('\r\n');
         const [method, path] = requestLine.split(' ');
-        console.log([requestLine, ...headers])
+        console.log([requestLine, ...headers][1])
         if (path === '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
         } else if (path.startsWith('/echo/')) {
