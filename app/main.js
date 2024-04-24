0efc470cd51dfc9460fe3a6d0049be57780d8e20
Josh Whitehead <Josh.whitehead30@gmail.com>
2024-04-24 12:38:58 -0700

    pass stage

--- app/main.js
+++ app/main.js
@@ -18,24 +18,14 @@ server.on('connection', function(socket) {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
 
         } else if (path.startsWith('/echo')) {
-            let arr = path.split('/').filter(elem => elem);
-            arr.shift();
-            const EchoString = arr.join('/');
-            let resp = 'HTTP/1.1 200 OK\r\n';
-            resp += 'Content-Type: text/plain\r\n';
-            resp += `Content-Length: ${EchoString.length}\r\n\r\n${EchoString}`;
-            socket.write(resp);
+            EchoRequest(path, socket);
             return
 
         }else if(path.startsWith('/user-agent')){
-            const UseragentStr = (chunk.toString().split('\r\n')[2].split(' ')[1]);
-            let resp = 'HTTP/1.1 200 OK\r\n';
-            resp += 'Content-Type: text/plain\r\n';
-            resp += `Content-Length: ${UseragentStr.length}\r\n\r\n${UseragentStr}`;
-            socket.write(resp);
+            UserAgentRequest(path, socket);
             return
         }else if(path.startsWith('/files')){
-            console.log("we got to the files area");
+            /*console.log("we got to the files area");
             FileStr = path.substring(path.lastIndexOf('/')+1);
             console.log(FileStr);
             fs.access(FileStr, fs.constants.F_OK, (err) => {
@@ -55,7 +45,7 @@ server.on('connection', function(socket) {
                     return
                 }
             });
-            
+            */
         } else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
             return
@@ -63,3 +53,19 @@ server.on('connection', function(socket) {
         }
     })
 })
+
+function EchoRequest(path, socket){
+    const Echostring = path.substring('/echo/'.length);
+    const EchoLength = Echostring.length;
+    const Response = `HTTP/1.1 200 OK\r\n Content-Type: text/plain\r\n Content-Length: ${EchoLength}\r\n\r\n${Echostring}`;
+    socket.write(Response);
+}
+function UserAgentRequest(path, socket){
+    const UserAgentStr = (chunk.toString().split('\r\n')[2].split(' ')[1]);
+    const UserAgentLength = UserAgentStr.length;
+    const Response = `HTTP/1.1 200 OK\r\n Content-Type: text/plain\r\n Content-Length: ${UserAgentLength}\r\n\r\n${UserAgentStr}`;
+    socket.write(Response);
+}
+function FileRequest(path, socket){
+
+}
